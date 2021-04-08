f84e05e30d6e6c9421965298e48b720f3cc97d90
Aleksandr Arsentev <justcruel@yandex.ru>
2021-04-08 10:29:24 +0300

    rename fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ang-orginizer app is running!');
+    expect(page.getTitleText()).toEqual('angular-orginizer app is running!');
   });
 
   afterEach(async () => {
